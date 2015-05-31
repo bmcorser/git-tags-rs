ca72d0d6d6884fa9c81f123b8e5db015fcae929b
bmcorser <bmcorser@gmail.com>
2015-05-31 22:49:30 +0100

    Use functions to namespace

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,13 +3,15 @@ extern crate clap;
 use clap::{App, SubCommand};
 
 mod release;
+mod lookup;
 
 fn main () {
     let app = App::new("git-tags")
                   .version("0.1.0")
                   .author("B M Corser <bmcorser@gmail.com>")
-                  .about("yaya")
-                  .subcommand(release::command);
+                  .about("Cut releases using Git tags.")
+                  .subcommand(release::command())
+                  .subcommand(lookup::command());
     let matches = app.get_matches();
     if let Some(matches) = matches.subcommand_matches("release") {
         if matches.is_present("message") {
