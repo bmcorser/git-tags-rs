997fc42ca0b9d55ae59777898a803a1461b3946a
bmcorser <bmcorser@gmail.com>
2015-05-31 21:35:14 +0100

    Realease usage

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,12 +2,22 @@ extern crate clap;
 
 use clap::{App, SubCommand};
 
-static DUMMY_USAGE: &'static str = "-m --message=[message] 'Tell others about your release'";
+static RELEASE_USAGE: &'static str = "
+-m --message=<message>  Tell others what this release is
+
+-a --alias=<alias>      Release named packages under an alias
+
+-r --repo=<repo>        Specifiy the repository to release from [default: ./]
+
+-n --no-remote          Don’t push tags to the remote
+
+-f --force              Make a release even if nothing changed
+";
 
 fn main() {
     let release_command = SubCommand::new("release")
                                      .about("about release")
-                                     .arg_from_usage(DUMMY_USAGE);
+                                     .arg_from_usage(RELEASE_USAGE);
     let app = App::new("git-tags")
                   .version("0.1.0")
                   .author("B M Corser <bmcorser@gmail.com>")
@@ -17,5 +27,4 @@ fn main() {
     if let Some(matches) = matches.subcommand_matches("release") {
         println!("{:?}",  matches.value_of("message"));
     }
-    // println!("{:?}", matches);
 }
