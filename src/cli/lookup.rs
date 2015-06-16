33025890ca94890514ca30d7754e631932b59f43
bmcorser <bmcorser@gmail.com>
2015-06-16 23:37:56 +0100

    Comment weirdness

--- src/cli/lookup.rs
+++ src/cli/lookup.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 use std::error::Error;
+use std::collections::{HashMap, HashSet};
 
 extern crate clap;
 extern crate git2;
@@ -8,17 +9,20 @@ use tag::lookup;
 use tag::error::LookupError;
 
 pub fn command<'a, 'b, 'c, 'd, 'e, 'f> () -> clap::App<'a, 'b, 'c, 'd, 'e, 'f> {
-    let usage = "\
+    let options_usage = "\
+    -l --list          'List all releases for package(s)'
     -r --repo=[repo]   'Specifiy the repository to release from'
     -y --yaml          'Output in YAML'";
+    /*
     let alias_usage = "-a --alias=[alias] 'Optionally lookup by alias'";
     let alias_arg = clap::Arg::from_usage(alias_usage)
                               .conflicts_with("pkg");
+    */
     clap::SubCommand::new("lookup")
                      .about("Get release tags by package name(s)")
                      .arg(clap::Arg::from_usage("<pkgs>.."))
-                     .args_from_usage(usage)
-                     .arg(alias_arg)
+                     .args_from_usage(options_usage)
+                     // .arg(alias_arg)
                      .arg_required_else_help(true)
 }
 
@@ -33,11 +37,27 @@ pub fn run(opts: &clap::ArgMatches) -> Result<(), LookupError> {
     };
     let pkg_names = opts.values_of("pkgs").unwrap();
     for pkg_name in pkg_names {
+        let mut revwalk = repo.revwalk().unwrap();
+        revwalk.push_head().unwrap();
+    }
+
+    for pkg_name in pkg_names {
+        println!("{:?}", pkg_name);
         let glob = format!("refs/tags/releases/{}/*", pkg_name);
+        let mut pkg_commits = HashSet::new();
         for reference in repo.references_glob(&glob).unwrap() {
-            // let ref_tree = reference.peel(git2::ObjectType::Tree).unwrap();
-            println!("{:?}", reference.name().unwrap());
+            let mut split_ref = Vec::with_capacity(5);
+            let ref_name = reference.name().unwrap();
+            split_ref.extend(ref_name.split("/"));
+            let commit = repo.revparse_single(split_ref[4]).unwrap();
+        }
+        let mut revwalk = repo.revwalk().unwrap();
+        revwalk.push_head().unwrap();
+        for reference in revwalk {
+            println!("{:?}", reference);
         }
     }
+    /**/
+
     Ok(())
 }
